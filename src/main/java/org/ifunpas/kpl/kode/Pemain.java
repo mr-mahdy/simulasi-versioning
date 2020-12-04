2fc5e3a44b79a9bcdf8e1f8dde6b3c0bd282f2b9
mr-mahdy <mr.mahdy5@gmail.com>
2020-12-04 14:41:36 +0700

    Menambahkan kelas pemain

---
+++ src/main/java/org/ifunpas/kpl/kode/Pemain.java
@@ -0,0 +1,12 @@
+package org.ifunpas.kpl.kode;
+
+public class Pemain {
+	private String nama;
+	private int noPunggung;
+	
+	public Pemain(String nama, int noPunggung) {
+		this.nama = nama;
+		this.noPunggung = noPunggung;
+	}
+	
+}
