62bee57a1570cfddfc88724e75b746acd7a1ed2c
aditiswadi <aditiswadi2@gmail.com>
2020-12-04 16:12:54 +0700

    Mengubah kelas Tim

--- src/main/java/org/ifunpas/kpl/kode/Tim.java
+++ src/main/java/org/ifunpas/kpl/kode/Tim.java
@@ -1,5 +1,17 @@
 package org.ifunpas.kpl.kode;
 
-public class Tim {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Tim {
+	private String nama;
+	private List<Pemain> pemainInti = new ArrayList<Pemain>();
+	
+	public Tim() {
+		nama = "";
+	}
+	
+	public Tim(String nama) {
+		this.nama = nama;
+	}
 }
